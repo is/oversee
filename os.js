06fac6ec445f0d98b6fec377f6d8fd20f2c11920
Xin Yu <scaner@gmail.com>
2012-11-04 14:07:54 +0800

    logging to console by log4js-node. #2

--- os.js
+++ os.js
@@ -1,3 +1,40 @@
 #!/usr/bin/env node
 
-require('./lib/index');
+var
+  common = require('./lib/common'),
+  core = require('./lib/index'),
+  OSTask = core.OSTask,
+  OSSlot = core.OSSlot,
+  OSContext = core.OSContext;
+
+common.getLogger('MAIN').info('START', {msg:'---------------------------------------------'});
+core.mlog.info('-- oversee.js --');
+
+// --- test codes ----
+var ctx = new OSContext("main");
+var slot = ctx.add(new OSSlot('main', 80));
+var slot = ctx.add(new OSSlot('default', 20));
+var task = ctx.add(new OSTask({
+  name: 't0', capacity:20, slot: 'default',
+  cmd: './s1.sh'}));
+
+ctx.add(new OSTask({
+  name: 't1', from:['t0'], 
+  capacity:20, slot: 'default',
+  cmd: './s2.sh'
+}));
+
+ctx.add(new OSTask({
+  name: 't2', from:['t0'], 
+  capacity:20, slot: 'default',
+  cmd: './s2.sh'
+}));
+
+ctx.add(new OSTask({
+  name: 't3', from:['t0'], 
+  capacity:20, slot: 'default',
+  cmd: './s2.sh'
+}));
+
+ctx.tidy();
+ctx.run();
